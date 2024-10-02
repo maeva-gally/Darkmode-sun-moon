76bd634177f908a2e5d470b2a98c3d46175f9950
maeva-gally <piti-bichoco@hotmail.fr>
2024-10-02 02:15:18 +0200

    initial upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function tdnn() {
+  document.getElementsByClassName("moon")[0].classList.toggle("sun");
+  document.getElementsByClassName("tdnn")[0].classList.toggle("day");
+  document.getElementsByTagName("BODY")[0].classList.toggle("light");
+}
